3ab608451d26b001874fae94ce951cfb5baa3f67
Bmorr1123 <33379673+Bmorr1123@users.noreply.github.com>
2023-09-26 10:55:27 -0500

    Fixes header fixer

--- update_headers.py
+++ update_headers.py
@@ -7,7 +7,7 @@ for team in lm.teams:
     read_file = open(f"data/teams/{id}.csv", "r")
     lines = [line for line in read_file.readlines() if line != '\n']  # Removes \n
     print(lines)
-    write_file = open(f"data/teams/{id}_new.csv", "w+")
+    write_file = open(f"data/teams/{id}.csv", "w+")
     write_file.write(",".join(lm.headers) + "\n")
     write_file.writelines(lines[1:])
 
